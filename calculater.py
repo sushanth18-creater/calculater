2251722d4b3f321655a72bac7ed1fd1077e69b74
sushanth18-creater <sushanthraj270@gmail.com>
2026-09-19 18:31:09 +0530

    added-onefile

---
+++ calculater.py
@@ -0,0 +1,16 @@
+a = int(input("enter the first number: "))
+b = int(input("enter the second number: " ))
+print("a.Addition")
+print("b.Substraction")
+print("c.multiplication")
+print("d.division")
+c = input("enter the opperater: ")
+if c == a:
+    print("addition:",a+b)
+elif c == b:
+    print("substrction:",a-b)
+elif c == c:
+    print("multiplication",a*b)
+else :
+    print("division:",a/b)
+print("Thank you bye")
